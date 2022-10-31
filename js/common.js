bb47c41ec30693c819113a1d52244c07f86e8456
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2022-10-31 21:14:59 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -130,6 +130,19 @@ document.addEventListener("DOMContentLoaded", function() {
         event.preventDefault();
     })
 
+    // ----------------------------------------------------------
+    const highlight = document.querySelector("#highlight-style");
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray(".text-highlight").forEach((highlight) => {
+        ScrollTrigger.create({
+            trigger: highlight,
+            start: "-100px center",
+            onEnter: () => highlight.classList.add("active")
+        });
+    });
+
 });
 
 // ---------------- after scrolling -------------------
