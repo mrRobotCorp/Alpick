9856e2853dc03b472a7efbc276665af16e1fcdbf
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2022-11-06 12:49:27 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+import scrollEvent from './scrollEvent.js';
+
+document.addEventListener("DOMContentLoaded", scrollEvent, function() {
     // ----------------- loader ------------------------------
     document.querySelector(".loading").style.visibility = "hidden";
     // -----------------------------------------------------
@@ -84,53 +86,53 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("mousemove", Trailer);
 
     // ------------------ scroll move ------------------------------
-    document.getElementById("logoTop").onclick = function () {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("logoTop").onclick = function () {
+    //     window.scrollTo({
+    //         top: 0,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
-    document.getElementById("menu0").onclick = function () {
-        window.scrollTo({
-            top: 750,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("menu0").onclick = function () {
+    //     window.scrollTo({
+    //         top: 750,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
-    document.getElementById("menu1").onclick = function () {
-        window.scrollTo({
-            top: 3150,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("menu1").onclick = function () {
+    //     window.scrollTo({
+    //         top: 3150,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
-    document.getElementById("menu2").onclick = function () {
-        window.scrollTo({
-            top: 9800,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("menu2").onclick = function () {
+    //     window.scrollTo({
+    //         top: 9800,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
-    document.getElementById("menu3").onclick = function () {
-        window.scrollTo({
-            top: 13550,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("menu3").onclick = function () {
+    //     window.scrollTo({
+    //         top: 13550,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
-    document.getElementById("scrollTop").onclick = function () {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+    // document.getElementById("scrollTop").onclick = function () {
+    //     window.scrollTo({
+    //         top: 0,
+    //         left: 0,
+    //         behavior: 'smooth'
+    //     });
+    // }
 
     // ---------------------------------------------------------
     const sticker0 = document.createElement('div');
@@ -164,81 +166,81 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // ------------------------------------------------------------
 
-    gsap.registerPlugin(ScrollTrigger);
+    // gsap.registerPlugin(ScrollTrigger);
 
-    const boxOR = gsap.timeline({
-        scrollTrigger: {
-        trigger: ".color", // 트리거 기준 요소 선택
-        start : "top 100%",
-        // pin: true,// 트리거 시작 지점
-        scrub: true,
-        end : "+=450 20%", // // scroll trigger와 동일함
-        // toggleActions : "play reverse none reverse"  // default : play none none none
-        // toggleActions : (onEnter, onLeave, onEnterBack, onLeaveBack) 값은 
-        // (play pause resume reset restart complete reverse none) 사용 가능  
-        // toggleActions : (트리거 시작할 때, 트리거 떠날 때, end방향으로 다시 진입할 때, start로 다시 진입할 때, start로 다시 나갈 때) 
-        }
-    });
+    // const boxOR = gsap.timeline({
+    //     scrollTrigger: {
+    //     trigger: ".color", // 트리거 기준 요소 선택
+    //     start : "top 100%",
+    //     // pin: true,// 트리거 시작 지점
+    //     scrub: true,
+    //     end : "+=450 20%", // // scroll trigger와 동일함
+    //     // toggleActions : "play reverse none reverse"  // default : play none none none
+    //     // toggleActions : (onEnter, onLeave, onEnterBack, onLeaveBack) 값은 
+    //     // (play pause resume reset restart complete reverse none) 사용 가능  
+    //     // toggleActions : (트리거 시작할 때, 트리거 떠날 때, end방향으로 다시 진입할 때, start로 다시 진입할 때, start로 다시 나갈 때) 
+    //     }
+    // });
 
-    const boxBlu = gsap.timeline({
-        scrollTrigger: {
-        trigger: ".color", // 트리거 기준 요소 선택
-        start : "top 100%",
-        scrub: true,
-        end : "+=450 20%", // // scroll trigger와 동일함
-        }
-    });
+    // const boxBlu = gsap.timeline({
+    //     scrollTrigger: {
+    //     trigger: ".color", // 트리거 기준 요소 선택
+    //     start : "top 100%",
+    //     scrub: true,
+    //     end : "+=450 20%", // // scroll trigger와 동일함
+    //     }
+    // });
 
-    const boxGry = gsap.timeline({
-        scrollTrigger: {
-        trigger: ".color", // 트리거 기준 요소 선택
-        start : "top 100%",
-        scrub: true,
-        end : "+=450 20%", // // scroll trigger와 동일함
-        }
-    });
+    // const boxGry = gsap.timeline({
+    //     scrollTrigger: {
+    //     trigger: ".color", // 트리거 기준 요소 선택
+    //     start : "top 100%",
+    //     scrub: true,
+    //     end : "+=450 20%", // // scroll trigger와 동일함
+    //     }
+    // });
 
-    const boxWh = gsap.timeline({
-        scrollTrigger: {
-        trigger: ".color", // 트리거 기준 요소 선택
-        start : "top 100%",
-        scrub: true,
-        end : "+=450 20%", // // scroll trigger와 동일함
-        }
-    });
+    // const boxWh = gsap.timeline({
+    //     scrollTrigger: {
+    //     trigger: ".color", // 트리거 기준 요소 선택
+    //     start : "top 100%",
+    //     scrub: true,
+    //     end : "+=450 20%", // // scroll trigger와 동일함
+    //     }
+    // });
 
     
     
-    boxOR.to('.sec8_colorOR', { 
-        // scrollTrigger:color, 
-        width:'60vw', 
-        duration:1
-    })
+    // boxOR.to('.sec8_colorOR', { 
+    //     // scrollTrigger:color, 
+    //     width:'60vw', 
+    //     duration:1
+    // })
 
-    boxBlu.to('.sec8_colorBlu', { 
-        // scrollTrigger:color, 
-        height:'33.3%', 
-        width: '40vw',
-        duration:1
-    })
+    // boxBlu.to('.sec8_colorBlu', { 
+    //     // scrollTrigger:color, 
+    //     height:'33.3%', 
+    //     width: '40vw',
+    //     duration:1
+    // })
 
-    boxGry.to('.sec8_colorGry', { 
-        // scrollTrigger:color, 
-        height:'33.3%', 
-        width: '40vw',
-        duration:1
-    })
+    // boxGry.to('.sec8_colorGry', { 
+    //     // scrollTrigger:color, 
+    //     height:'33.3%', 
+    //     width: '40vw',
+    //     duration:1
+    // })
 
-    boxWh.to('.sec8_colorWh', { 
-        // scrollTrigger:color, 
-        height:'33.3%', 
-        width: '40vw',
-        duration:1
-    })
+    // boxWh.to('.sec8_colorWh', { 
+    //     // scrollTrigger:color, 
+    //     height:'33.3%', 
+    //     width: '40vw',
+    //     duration:1
+    // })
 
 });
 
-    AOS.init();
+    // AOS.init();
 
     // ---------------- scrolling -------------------
     $(document).ready(function(){
--- js/scrollEvent.js
+++ js/scrollEvent.js
@@ -1,7 +1,126 @@
-const controller = new ScrollMagic.controller();
+export default addEventListener();
 
-const colorLeft = TweenMax.to('.sec8_colorOR', 0.5, {
-    width: '60vw'
-})
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+    // ------------------ scroll move ------------------------------
+    document.getElementById("logoTop").onclick = function () {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
 
-const scene = new ScrollMagic.Scene().setTween(tween1);
+    document.getElementById("menu0").onclick = function () {
+        window.scrollTo({
+            top: 750,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    document.getElementById("menu1").onclick = function () {
+        window.scrollTo({
+            top: 3150,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    document.getElementById("menu2").onclick = function () {
+        window.scrollTo({
+            top: 9800,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    document.getElementById("menu3").onclick = function () {
+        window.scrollTo({
+            top: 13550,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    document.getElementById("scrollTop").onclick = function () {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    // -----------------------------------------------------------------
+    gsap.registerPlugin(ScrollTrigger);
+
+    const boxOR = gsap.timeline({
+        scrollTrigger: {
+        trigger: ".color", // 트리거 기준 요소 선택
+        start : "top 100%",
+        // pin: true,// 트리거 시작 지점
+        scrub: true,
+        end : "+=450 20%", // // scroll trigger와 동일함
+        // toggleActions : "play reverse none reverse"  // default : play none none none
+        // toggleActions : (onEnter, onLeave, onEnterBack, onLeaveBack) 값은 
+        // (play pause resume reset restart complete reverse none) 사용 가능  
+        // toggleActions : (트리거 시작할 때, 트리거 떠날 때, end방향으로 다시 진입할 때, start로 다시 진입할 때, start로 다시 나갈 때) 
+        }
+    });
+
+    const boxBlu = gsap.timeline({
+        scrollTrigger: {
+        trigger: ".color", // 트리거 기준 요소 선택
+        start : "top 100%",
+        scrub: true,
+        end : "+=450 20%", // // scroll trigger와 동일함
+        }
+    });
+
+    const boxGry = gsap.timeline({
+        scrollTrigger: {
+        trigger: ".color", // 트리거 기준 요소 선택
+        start : "top 100%",
+        scrub: true,
+        end : "+=450 20%", // // scroll trigger와 동일함
+        }
+    });
+
+    const boxWh = gsap.timeline({
+        scrollTrigger: {
+        trigger: ".color", // 트리거 기준 요소 선택
+        start : "top 100%",
+        scrub: true,
+        end : "+=450 20%", // // scroll trigger와 동일함
+        }
+    });
+
+    
+    
+    boxOR.to('.sec8_colorOR', { 
+        // scrollTrigger:color, 
+        width:'60vw', 
+        duration:1
+    })
+
+    boxBlu.to('.sec8_colorBlu', { 
+        // scrollTrigger:color, 
+        height:'33.3%', 
+        width: '40vw',
+        duration:1
+    })
+
+    boxGry.to('.sec8_colorGry', { 
+        // scrollTrigger:color, 
+        height:'33.3%', 
+        width: '40vw',
+        duration:1
+    })
+
+    boxWh.to('.sec8_colorWh', { 
+        // scrollTrigger:color, 
+        height:'33.3%', 
+        width: '40vw',
+        duration:1
+    })
+});
