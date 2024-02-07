2c00698e189360e8280d6fcc607ce6e6b01b4878
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2024-02-07 13:05:32 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     AOS.init();
 
     // ----------------- loader ------------------------------
@@ -13,14 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
         </div>
     </div>
     `);
-    
+
     document.querySelector(".loading").style.visibility = "hidden";
-    
+
     // ------------------- hamburger menu -----------------------------
     var trigerMenu = document.querySelector("#menu");
     var gnbMenu = document.getElementById("gnb");
 
-    trigerMenu.onclick = function() {
+    trigerMenu.onclick = function () {
         gnbMenu.style.transition = "all .5s"
         trigerMenu.classList.toggle("open");
         this.classList.toggle("active");
@@ -30,18 +30,18 @@ document.addEventListener("DOMContentLoaded", function() {
     // --------------------- header interaction ---------------------------
     const scrollNum = document.querySelector("html").scrollTop;
 
-    window.onresize = function(event){
+    window.onresize = function (event) {
         const innerWidth = window.innerWidth;
 
         if (innerWidth > 1024) {
-            window.onscroll = function() {
+            window.onscroll = function () {
 
-                if ( scrollNum === 0) {
+                if (scrollNum === 0) {
                     document.querySelector("header").style.top = "0";
                     document.querySelector("header div").style.marginBottom = "calc(100vw * 17 / 1920)";
                 }
-                
-                if ( scrollNum > 740) {
+
+                if (scrollNum > 740) {
                     document.querySelector("header").style.top = "calc(100vw * -31 / 1920)";
                     document.querySelector("header div").style.marginBottom = "0";
                 } else {
@@ -50,8 +50,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
 
             }
-            window.onmousemove = function(e) {
-                if ( e.clientY < 70 ) {
+            window.onmousemove = function (e) {
+                if (e.clientY < 70) {
                     document.querySelector("header").style.top = "0";
                     document.querySelector("header div").style.marginBottom = "calc(100vw * 17 / 1920)";
                 } else if (scrollNum > 740) {
@@ -66,10 +66,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // -------------------------- progress indicator ---------------------------------
     window.addEventListener("scroll", () => {
-        if(document.querySelector('.progressbar') != null)setProgress();
-    }); 
+        if (document.querySelector('.progressbar') != null) setProgress();
+    });
 
-    function setProgress() {          
+    function setProgress() {
         let currY = document.documentElement.scrollTop;
         let totalY = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         let percentage = (currY / totalY) * 100;
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("mousemove", myCursor);
     window.addEventListener("mousemove", Trailer);
-    
+
     if (window.innerWidth < 768) {
         cursor.style.visibility = 'hidden';
         follower.style.visibility = 'hidden';
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // sticker4.setAttribute('class', 'sticker');
 
     // stickerAll.prepend(sticker0, sticker1, sticker2, sticker3, sticker4);
-    
+
     // ------------------ prevent event ------------------------------
     const mainMockup = document.querySelector('#mainMockup');
     mainMockup.addEventListener('contextmenu', (event) => {
@@ -190,82 +190,128 @@ document.addEventListener("DOMContentLoaded", function() {
             behavior: 'smooth'
         });
     };
-    
+
     // --------------------------- scrollTrigger --------------------------------------
     gsap.registerPlugin(ScrollTrigger);
 
     const boxOR = gsap.timeline({
         scrollTrigger: {
-        trigger: ".allColor",
-        start : "top 90%",
-        scrub: true,
-        end : "+=200 20%"
+            trigger: ".allColor",
+            start: "top 90%",
+            scrub: true,
+            end: "+=200 20%"
         }
     });
 
     const boxBlu = gsap.timeline({
         scrollTrigger: {
-        trigger: ".allColor",
-        start : "top 90%",
-        scrub: true,
-        end : "+=200 20%"
+            trigger: ".allColor",
+            start: "top 90%",
+            scrub: true,
+            end: "+=200 20%"
         }
     });
 
     const boxGry = gsap.timeline({
         scrollTrigger: {
-        trigger: ".allColor",
-        start : "top 70%",
-        scrub: true,
-        end : "+=200 13%"
+            trigger: ".allColor",
+            start: "top 70%",
+            scrub: true,
+            end: "+=200 13%"
         }
     });
 
     const boxWh = gsap.timeline({
         scrollTrigger: {
-        trigger: ".allColor",
-        start : "top 50%",
-        scrub: true,
-        end : "+=200 7%"
+            trigger: ".allColor",
+            start: "top 50%",
+            scrub: true,
+            end: "+=200 7%"
         }
     });
-    
-    boxOR.to('.sec8_colorOR', { 
-        width:'53vw', 
-        duration:1
+
+    boxOR.to('.sec8_colorOR', {
+        width: '53vw',
+        duration: 1
     });
 
-    boxBlu.to('.sec8_colorBlu', { 
-        height:'33.3%', 
+    boxBlu.to('.sec8_colorBlu', {
+        height: '33.3%',
         width: '38vw',
-        duration:1
+        duration: 1
     });
 
-    boxGry.to('.sec8_colorGry', { 
-        height:'33.3%', 
+    boxGry.to('.sec8_colorGry', {
+        height: '33.3%',
         width: '38vw',
-        duration:1
+        duration: 1
     });
 
-    boxWh.to('.sec8_colorWh', { 
-        height:'33.3%', 
+    boxWh.to('.sec8_colorWh', {
+        height: '33.3%',
         width: '38vw',
-        duration:1
+        duration: 1
     });
+
+
+
+    if (window.innerWidth < 768) {
+            /*
+              Quick whip-up of an idea posed by a client: a bar filled with logo's that move to the left slowly and infinitely.
+              I checked if the <marquee> tag was still working (and it is), but it's considered invalid html now so I needed something else.
+            */
+
+            const items = [...document.getElementsByClassName('list__item')];
+            const containerElem = document.getElementById('containerElem');
+            const leftSideOfContainer = containerElem.getBoundingClientRect().left;
+            const listElem = document.getElementById('list');
+            let currentLeftValue = 0;
+
+            // Kick off for the animation function.
+            window.setInterval(animationLoop, 40);
+
+            /* 
+              Looks at first item in the list and checks if it goes out of the visible area 
+              by comparing the right position of the first list item to the left position 
+              of the containing element. 
+            */
+            function animationLoop() {
+                const firstListItem = listElem.querySelector('.list__item:first-child');
+
+                let rightSideOfFirstItem = firstListItem.getBoundingClientRect().right;
+
+                /* 
+                  If first list item is out of viewable area, move it to the end of the list. 
+                  Also, set the current left value to -1 so we won't stutter.
+                */
+                if (rightSideOfFirstItem == leftSideOfContainer) {
+                    currentLeftValue = -1;
+                    listElem.appendChild(firstListItem);
+                }
+
+                // The part that keeps it all going: animating the margin left value of the list.
+                listElem.style.marginLeft = `${currentLeftValue}px`;
+                currentLeftValue--;
+            }
+        
+    }
+
+
 
 });
 
 
 // -------------------------------------------------------------------
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const Height = $("#scrollTop").height(); 
+    const Height = $("#scrollTop").height();
     $("#scrollTop").hide();
 
-    $(window).scroll(function(){ 
-        var rolling = $(this).scrollTop() >= Height; 
-        if (rolling) { 
-            $("#scrollTop").fadeIn(500).css({"position":"fixed"}); } 
+    $(window).scroll(function () {
+        var rolling = $(this).scrollTop() >= Height;
+        if (rolling) {
+            $("#scrollTop").fadeIn(500).css({ "position": "fixed" });
+        }
         else {
             $("#scrollTop").fadeOut(300);
         }
@@ -274,57 +320,57 @@ $(document).ready(function(){
 
     // ----------------------- loop slide ---------------------- 
     (function ($) {
-        $.fn.marquee = function( setting ){
+        $.fn.marquee = function (setting) {
 
-            function rightToLeft( wrap, content, speed ){
+            function rightToLeft(wrap, content, speed) {
                 var width = 0;
-                content.append( content.html() );
-                content.append( content.html() );
+                content.append(content.html());
+                content.append(content.html());
                 content.find('dl').each(function () {
                     width += $(this).outerWidth();
                 })
 
-                content.width( width );
+                content.width(width);
 
                 function run() {
-                    if( wrap.scrollLeft() - (content.width() / 2) >= 0 ){
-                        wrap.scrollLeft( 0 );
-                    }else{
-                        wrap.scrollLeft( wrap.scrollLeft() + 1 );
+                    if (wrap.scrollLeft() - (content.width() / 2) >= 0) {
+                        wrap.scrollLeft(0);
+                    } else {
+                        wrap.scrollLeft(wrap.scrollLeft() + 1);
                     }
                 }
 
-                var timer = setInterval( run, speed );
+                var timer = setInterval(run, speed);
 
-                wrap.mouseover( function () {
-                    clearInterval( timer );
-                } )
-                wrap.mouseout( function () {
-                    timer = setInterval( run, speed );
-                } )
+                wrap.mouseover(function () {
+                    clearInterval(timer);
+                })
+                wrap.mouseout(function () {
+                    timer = setInterval(run, speed);
+                })
 
             }
 
-            return this.each(function(){
+            return this.each(function () {
                 var wrap = $(this);
-                var content = wrap.find( setting.container );
+                var content = wrap.find(setting.container);
                 var animateType = wrap.attr('type');
 
-                if ( animateType == 'right-to-left' ){
-                    content.addClass( animateType );
-                    rightToLeft( wrap, content, setting.speed );
+                if (animateType == 'right-to-left') {
+                    content.addClass(animateType);
+                    rightToLeft(wrap, content, setting.speed);
                 }
             })
         }
 
-        })(jQuery)
+    })(jQuery)
 
-        var setting = {
+    var setting = {
         container: '.marquee-content',
         speed: 0.6
-        }
+    }
 
-        $('.marquee-wrap').marquee( setting );
+    $('.marquee-wrap').marquee(setting);
 
     // -----------------------  audio control --------------------------
     const audio0 = document.querySelector('#sec7_audio0');
@@ -332,8 +378,8 @@ $(document).ready(function(){
     audio0.volume = 0.8;
     audio1.volume = 0.8;
 
-    $('.sec7_box0').on('click',function(){
-        if($(this).attr('data-click') == 1) {
+    $('.sec7_box0').on('click', function () {
+        if ($(this).attr('data-click') == 1) {
             $(this).attr('data-click', 0);
             $('#sec7_audio0')[0].pause();
         } else {
@@ -342,8 +388,8 @@ $(document).ready(function(){
         }
     });
 
-    $('.sec7_box2').on('click',function(){
-        if($(this).attr('data-click') == 1) {
+    $('.sec7_box2').on('click', function () {
+        if ($(this).attr('data-click') == 1) {
             $(this).attr('data-click', 0);
             $('#sec7_audio1')[0].pause();
         } else {
@@ -351,5 +397,5 @@ $(document).ready(function(){
             $('#sec7_audio1')[0].play();
         }
     });
-    
+
 });
